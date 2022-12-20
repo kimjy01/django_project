def957b9a50ac9144a3e7b20e5cd810498248c7f
kimjy01 <jsujyeon@naver.com>
2022-12-20 23:25:23 +0900

    test

--- blog/models.py
+++ blog/models.py
@@ -12,6 +12,8 @@ class Post(models.Model):
         default=timezone.now)
     published_date = models.DateTimeField(
         blank=True, null=True)
+    like = models.IntegerField(null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect,HttpResponseRedirect
 from .forms import PostForm
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -27,6 +31,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -40,3 +45,16 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+@csrf_exempt
+def vote(request, pk):
+    try:
+        post = get_object_or_404(Post, pk=pk)
+    except(KeyError, Post.DoesNotExist):
+        return render(request, 'blog/post_detail.html', {'post': post, 'error_message': "You didn't select a choice."})
+    else:
+        post.like += 1
+        post.save()
+
+    return render(request, 'blog/post_detail.html', {'post': post})
